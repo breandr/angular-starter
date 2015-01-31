af8ddfa7b31d69a0cc18bc80ada6312c550fd58a
Brett Andrews <brett.j.andrews@gmail.com>
2015-01-31 12:55:46 -0800

    add gulp-bower to remove global bower dependency

--- gulp-tasks/bower.js
+++ gulp-tasks/bower.js
@@ -1,8 +1,15 @@
 var paths = require('./config.js').paths,
   gulp = require('gulp'),
+  bower = require('gulp-bower'),
   wiredep = require('wiredep').stream;
 
 // 1. Write bower components to index.jade
+gulp.task('bower-install', function() {
+  return bower();
+});
+gulp.task('bower-update', function() {
+  return bower({ cmd: 'update'});
+});
 gulp.task('bower', function() {
   return gulp.src(paths.src.root + 'index.jade')
     .pipe(wiredep({
